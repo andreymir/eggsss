80baa050eac346098e194912fbf65c60d60e871d
VladimirN <nachatoy@gmail.com>
2012-11-03 13:41:31 +0400

    catcher

---
+++ eggsss/eggsss/Catcher.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace eggsss
+{
+    public class Catcher
+    {
+        public CatcherState State { get; set; }
+
+        // Animation representing the player
+        public Texture2D[] PlayerTexture;
+
+        // Position of the Player relative to the upper left side of the screen
+        public Vector2 Position;
+
+        // State of the player
+        public bool Active;
+
+        public void Initialize(Texture2D[] texture, Vector2 position, CatcherState state)
+        {
+            PlayerTexture = texture;
+
+            Position = position;
+
+            // Set the starting position of the player around the middle of thescreen and to the back
+            State = state;
+
+            // Set the player to be active
+            Active = true;
+        }
+
+
+        public void Update(CatcherState state)
+        {
+            State = state;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Texture2D bodyTexture = null;
+            Texture2D basketTexture = null;
+            Vector2 bodyPosition = new Vector2();
+            Vector2 basketPosition = new Vector2();
+
+            switch (State)
+            {
+                case CatcherState.TopLeft:
+                    bodyTexture = PlayerTexture[0];
+                    basketTexture = PlayerTexture[2];
+                    bodyPosition = Position + new Vector2(-bodyTexture.Width, -bodyTexture.Height);
+                    basketPosition = Position + new Vector2(-bodyTexture.Width - basketTexture.Width, -bodyTexture.Height);
+                    break;
+                case CatcherState.BottomLeft:
+                    bodyTexture = PlayerTexture[0];
+                    basketTexture = PlayerTexture[3];
+                    bodyPosition = Position + new Vector2(-bodyTexture.Width, -bodyTexture.Height);
+                    basketPosition = Position + new Vector2(-bodyTexture.Width - basketTexture.Width, -bodyTexture.Height + basketTexture.Height);
+                    break;
+                case CatcherState.TopRight:
+                    bodyTexture = PlayerTexture[1];
+                    basketTexture = PlayerTexture[4];
+                    bodyPosition = Position + new Vector2(0, -bodyTexture.Height);
+                    basketPosition = Position + new Vector2(basketTexture.Width * 0.25f + basketTexture.Width, -bodyTexture.Height);
+                    break;
+                case CatcherState.BottomRight:
+                    bodyTexture = PlayerTexture[1];
+                    basketTexture = PlayerTexture[5];
+                    bodyPosition = Position + new Vector2(0, -bodyTexture.Height);
+                    basketPosition = Position + new Vector2(basketTexture.Width * 0.95f, -bodyTexture.Height + basketTexture.Height * 1.1f);
+                    break;
+
+            }
+
+            if (bodyTexture == null || basketTexture == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            spriteBatch.Draw(bodyTexture, bodyPosition, Color.White);
+            spriteBatch.Draw(basketTexture, basketPosition, Color.White);
+        }
+    }
+}
--- eggsss/eggsss/Game1.cs
+++ eggsss/eggsss/Game1.cs
@@ -18,6 +18,7 @@ namespace eggsss
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        private Catcher cather;
 
         public Game1()
         {
@@ -35,6 +36,9 @@ namespace eggsss
         {
             // TODO: Add your initialization logic here
 
+            //Initialize the player class
+            cather = new Catcher();
+
             base.Initialize();
         }
 
@@ -46,6 +50,21 @@ namespace eggsss
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            var playerPosition =
+                new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X + GraphicsDevice.Viewport.TitleSafeArea.Width / 2, GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height);
+
+            cather.Initialize(
+                new[] {
+                    Content.Load<Texture2D>("Catcher/catcher-left"),
+                    Content.Load<Texture2D>("Catcher/catcher-right"),
+                    Content.Load<Texture2D>("Catcher/catcher-ltop"),
+                    Content.Load<Texture2D>("Catcher/catcher-lbottom"),
+                    Content.Load<Texture2D>("Catcher/catcher-rtop"),
+                    Content.Load<Texture2D>("Catcher/catcher-rbottom"),
+            },
+            playerPosition,
+            CatcherState.TopLeft);
 
             // TODO: use this.Content to load your game content here
         }
@@ -67,12 +86,78 @@ namespace eggsss
         protected override void Update(GameTime gameTime)
         {
             // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            if (Keyboard.GetState().GetPressedKeys().FirstOrDefault() == Keys.Escape)
                 this.Exit();
 
-            // TODO: Add your update logic here
+            // Read the current state of the keyboard and gamepad and store it
+            //currentKeyboardState = Keyboard.GetState();
+
+
+            //Update the player
+            UpdateCatcher(gameTime);
+
 
             base.Update(gameTime);
+        }
+
+        private void UpdateCatcher(GameTime gameTime)
+        {
+            var keyboardState = Keyboard.GetState();
+            var state = CatcherState.Unknown;
+
+            if (keyboardState.IsKeyDown(Keys.NumPad4))
+            {
+                state = CatcherState.TopLeft;
+            }
+            else if (keyboardState.IsKeyDown(Keys.NumPad5))
+            {
+                state = CatcherState.TopRight;
+            }
+            else if (keyboardState.IsKeyDown(Keys.NumPad1))
+            {
+                state = CatcherState.BottomLeft;
+            }
+            else if (keyboardState.IsKeyDown(Keys.NumPad2))
+            {
+                state = CatcherState.BottomRight;
+            }
+
+            if (state != CatcherState.Unknown)
+            {
+                cather.Update(state);
+            }
+
+            //// Get Thumbstick Controls
+            //player.Position.X += currentGamePadState.ThumbSticks.Left.X * playerMoveSpeed;
+            //player.Position.Y -= currentGamePadState.ThumbSticks.Left.Y * playerMoveSpeed;
+
+            //// Use the Keyboard / Dpad
+            //if (currentKeyboardState.IsKeyDown(Keys.Left) ||
+            //currentGamePadState.DPad.Left == ButtonState.Pressed)
+            //{
+            //    player.Position.X -= playerMoveSpeed;
+            //}
+            //if (currentKeyboardState.IsKeyDown(Keys.Right) ||
+            //currentGamePadState.DPad.Right == ButtonState.Pressed)
+            //{
+            //    player.Position.X += playerMoveSpeed;
+            //}
+            //if (currentKeyboardState.IsKeyDown(Keys.Up) ||
+            //currentGamePadState.DPad.Up == ButtonState.Pressed)
+            //{
+            //    player.Position.Y -= playerMoveSpeed;
+            //}
+            //if (currentKeyboardState.IsKeyDown(Keys.Down) ||
+            //currentGamePadState.DPad.Down == ButtonState.Pressed)
+            //{
+            //    player.Position.Y += playerMoveSpeed;
+            //}
+
+
+            //// Make sure that the player does not go out of bounds
+            //player.Position.X = MathHelper.Clamp(player.Position.X, 0, GraphicsDevice.Viewport.Width - player.Width);
+            //player.Position.Y = MathHelper.Clamp(player.Position.Y, 0, GraphicsDevice.Viewport.Height - player.Height);
+
         }
 
         /// <summary>
@@ -83,7 +168,14 @@ namespace eggsss
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            // Start drawing
+            spriteBatch.Begin();
+
+            // Draw the Player
+            cather.Draw(spriteBatch);
+
+            //Stop drawing
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
