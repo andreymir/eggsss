09a26aa2151cd2b64c6ee9bf9aaddb510ef45282
VladimirN <nachatoy@gmail.com>
2012-11-03 21:44:39 +0400

    1

--- eggsss/eggsss/Game1.cs
+++ eggsss/eggsss/Game1.cs
@@ -204,6 +204,29 @@ namespace eggsss
                 paused = false;
             }
 
+            if (kinectStartState == StartResult.ManyKinectsDetected || kinectStartState == StartResult.KinectError)
+                paused = true;
+
+            if (kinectStartState == StartResult.KinectStarted)
+            {
+                var kinectCurrentVoice = kinect.GetCurrentVoiceCommand();
+                switch (kinectCurrentVoice)
+                {
+                    case VoiceCommand.NewGame:
+                        Restart();
+                        break;
+                    case VoiceCommand.Exit:
+                        Exit();
+                        break;
+                    case VoiceCommand.Pause:
+                        paused = true;
+                        break;
+                    case VoiceCommand.Continue:
+                        paused = false;
+                        break;
+                }
+            }
+
             if (paused)
             {
                 pauseButton.Visible = false;
@@ -356,23 +379,6 @@ namespace eggsss
                         break;
                     case GameState.FourthAreaSelected:
                         state = CatcherState.BottomRight;
-                        break;
-                }
-
-                var kinectCurrentVoice = kinect.GetCurrentVoiceCommand();
-                switch (kinectCurrentVoice)
-                {
-                    case VoiceCommand.NewGame:
-                        Restart();
-                        break;
-                    case VoiceCommand.Exit:
-                        Exit();
-                        break;
-                    case VoiceCommand.Pause:
-                        isPause = true;
-                        break;
-                    case VoiceCommand.Continue:
-                        isPause = false;
                         break;
                 }
             }
